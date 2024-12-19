faefe127c2dc863fd966cdfc991a399d3bb27f12
yu <yul1024@outlook.com>
2024-12-19 16:13:45 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,9 +3,11 @@ conda env list
 
 
 # rebuild env
-# if exist env
+
+# if exist the old env, remove the env first.
 conda deactivate
 conda remove --name ds_env --all  # need to change the env name here
+
 
 # create env by configuration file
 conda env create -f ./environment.yaml
@@ -14,3 +16,4 @@ conda env create -f ./environment.yaml
 # check env
 conda activate ds_env  # need to change the env name here
 conda list
+
