3d56643202cbf5acdb5f9446e4fd1ff9a554ac02
yu <yul1024@outlook.com>
2025-04-03 21:22:03 +0800

    Update llama_factory_env.sh

--- specific_projects/llama_factory/llama_factory_env.sh
+++ specific_projects/llama_factory/llama_factory_env.sh
@@ -1,5 +1,13 @@
+# create env and install python
+conda create -n llama_factory_env python=3.12
 
+# activate env
+conda activate llama_factory_env
 
+# auto script
+# git clone --depth 1 https://github.com/hiyouga/LLaMA-Factory.git  # need to clone the repo first
+cd path/to/lama_factory_repo
+pip install -e ".[torch,metrics]"
 
-
-
+# env check
+llamafactory-cli version
